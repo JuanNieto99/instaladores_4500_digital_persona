6132cbc01fc34af3ec489deb6f38b4a67a962c84
Jorge Martinez <jo.cabmartin2@gmail.com>
2023-11-03 11:50:51 -0500

    Añadido validación y habilitación de privilegios de de administrador

--- windows/install_service.bat
+++ windows/install_service.bat
@@ -2,6 +2,30 @@
    
    CALL :main
 
+    :check_admin
+        net session >nul 2>&1
+        set adminStatus=%errorlevel%
+        
+        exit /B
+
+    :enable_admin
+        set /p answer=Para continuar debe Habilitar permisos de administrador ¿Desea continuar? (y/n): 
+        
+        if /i "%answer%"=="y" (
+            NET FILE 1>NUL 2>NUL || (
+                powershell -Command "Start-Process -Verb RunAs -FilePath '%~dpnx0' -ArgumentList 'am_admin'"
+                exit
+            )
+        ) else if /i "%answer%"=="n" (
+                echo No se han asignados privilegios de administrador.
+                echo El terminal se cerrará en 5 segundos. Pulse cualquier tecla para cerrarlo inmediatamente.
+                timeout /nobreak /t 5 >nul
+                choice /n /c Y /t 0 /d Y >nul
+            exit
+        ) 
+
+        exit /b
+
     :install_wsl_update
     
         echo Instalando Windows Subsystem for Linux (WSL)...
@@ -105,10 +129,21 @@
         pause
     EXIT /B
 
- :main
+
+    :start_process
         CALL :install_wsl_update
         CALL :startup_files
         CALL :install_sdk
         CALL :install_dorssel
         CALL :create_task_manger 
+        exit /b
+
+
+    :main
+        
+        title Fingerprint installer - Windows
+
+        call :check_admin adminStatus
+    
+        if "%adminStatus%"=="0" (call:start_process) else (call:enable_admin)    
  exit /b
